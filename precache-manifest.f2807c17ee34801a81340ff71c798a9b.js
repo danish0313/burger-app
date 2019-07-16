d2fa390132cacec44315d2dd57e8b49b28d52cde
danish0313 <danishoo.dk@gmail.com>
2019-07-17 02:39:45 +0500

    Updates

---
+++ precache-manifest.f2807c17ee34801a81340ff71c798a9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b020865df37abf2c13462e27fa26b72",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "e03c6776114e3aca4eaa",
+    "url": "/burger-app/static/css/3.3cf7562e.chunk.css"
+  },
+  {
+    "revision": "74306f72a62ab416e4d9",
+    "url": "/burger-app/static/css/4.5dd790fb.chunk.css"
+  },
+  {
+    "revision": "27b121b2cf0132a8e1c1",
+    "url": "/burger-app/static/css/main.f520af17.chunk.css"
+  },
+  {
+    "revision": "e0832d80f5ed2c24f6a0",
+    "url": "/burger-app/static/js/2.c318eac1.chunk.js"
+  },
+  {
+    "revision": "e03c6776114e3aca4eaa",
+    "url": "/burger-app/static/js/3.9e05f777.chunk.js"
+  },
+  {
+    "revision": "74306f72a62ab416e4d9",
+    "url": "/burger-app/static/js/4.8274b788.chunk.js"
+  },
+  {
+    "revision": "a6b9c5373f99f395e42a",
+    "url": "/burger-app/static/js/5.0b05985e.chunk.js"
+  },
+  {
+    "revision": "27b121b2cf0132a8e1c1",
+    "url": "/burger-app/static/js/main.7e96e514.chunk.js"
+  },
+  {
+    "revision": "13bb31cdaa7140e4a252",
+    "url": "/burger-app/static/js/runtime~main.89ca34a3.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
